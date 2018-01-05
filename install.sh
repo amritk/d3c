345c2622708abd1aba542d31a9c0a404b5e0a828
DESMAREZ BENJAMIN <benjamin.desmarez@decathlon.com>
2018-01-05 15:00:53 +0100

    feat(code): update install script

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 echo "Installing Commitizen Globally"
 npm install -g commitizen
 echo "Installing JIRA smart commits"
-npm install -g git@git.osiris.withoxylane.com:crm/d3c.git
+npm install -g  "git+ssh://git@git.osiris.withoxylane.com:crm/d3c.git"
 echo "Creating a global config file"
 echo '{ "path": "/usr/local/lib/node_modules/d3c/" }' > ~/.czrc
